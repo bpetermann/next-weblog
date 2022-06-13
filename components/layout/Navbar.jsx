c16f262e0cce797ba8ae4759a96a70c5e0c97775
Benjamin Petermann <benjamin.petermann@gmx.at>
2022-06-13 23:52:56 +0200

    Added links and styles to navbar

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -1,19 +1,38 @@
 import styles from './Navbar.module.css';
+import Link from 'next/link';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { AiOutlineSearch } from 'react-icons/ai';
 
 const Navbar = () => {
   return (
-    <header className={styles['header']}>
-      <div className={styles['navbar-left-content']}>
-        <GiHamburgerMenu size={26} className={styles['hamburger']} />
-        <h3 className={styles['logo']}>
-          <span>next</span>Blog
-        </h3>
-      </div>
-      <div className={styles['searchbar']}>
-        <AiOutlineSearch size={26} className={styles['search-icon']} />
-        <input className={styles['search']} type='text' placeholder='Search...' />
+    <header className={styles['fixed']}>
+      <div className={styles['navbar']}>
+        <div className={styles['navbar-left']}>
+          <GiHamburgerMenu size={26} className={styles['hamburger']} />
+          <Link href='/'>
+            <a className={styles['logo']}>
+              <h2>
+                <span>next</span>Blog
+              </h2>
+            </a>
+          </Link>
+        </div>
+
+        <div className={styles['navbar-center']}>
+          <Link href='/posts'>
+            <a className={styles['all-posts-link']}>
+              <h2>All Posts</h2>
+            </a>
+          </Link>
+        </div>
+        <div className={styles['searchbar']}>
+          <AiOutlineSearch size={26} className={styles['search-icon']} />
+          <input
+            className={styles['search']}
+            type='text'
+            placeholder='Search...'
+          />
+        </div>
       </div>
     </header>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <div>
-      <h1 style={{ margin: '1rem' }}>The Home Page</h1>
+      <h1 style={{ margin: '1rem', marginTop: '5rem' }}>Home Page</h1>
     </div>
   );
 };
